490f7827e80035c21c03e38ec5a482b9c18c1ddd
Soumen Das <dassoumen9832@gmail.com>
2023-07-02 12:35:47 +0530

    day 1 completed

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const serverConfig = require('./configs/server.config')
+
+const app = express();
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}`);
+})
